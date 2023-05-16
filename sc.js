72176a058858fb139b5b5ade8dc82637fbd54f9b
vinayak <vinayak>
2023-05-16 08:51:23 +0530

    add

---
+++ sc.js
@@ -0,0 +1,37 @@
+const inputbox=document.getElementById('input-box');
+const listContainer=document.getElementById("list-container");
+ function addTask(){
+if(inputbox.value === ''){
+    alert("you must write smtg");
+}
+else{
+    let li=document.createElement('li')
+    li.innerHTML=inputbox.value;
+    listContainer.appendChild(li);
+    let span=document.createElement("span");
+    span.innerHTML="\u00d7";
+    li.appendChild(span)
+}
+
+inputbox.value="";
+save();
+ }
+ listContainer.addEventListener('click',(e)=>{
+
+    if(e.target.tagName ==='LI'){
+        e.target.classList.toggle('checked');
+        save();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        save();
+    }
+ },false);
+ function save(){
+    localStorage.setItem("data",listContainer.innerHTML);
+
+ }
+ function show(){
+    listContainer.innerHTML=localStorage.getItem("data");
+ }
+ show()
